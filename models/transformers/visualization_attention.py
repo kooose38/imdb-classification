e1ab4533c20dbde7f4b2499f6cb61f1527177b7b
kooose38 <kooose0308@gmail.com>
2021-08-07 16:28:04 +0900

    [FIX]

--- models/transformers/visualization_attention.py
+++ models/transformers/visualization_attention.py
@@ -1,9 +1,15 @@
+import json 
 def _highlight(word: str, attn: float) -> str:
     "Attentionの値が大きいと文字の背景が濃い赤になるhtmlを出力させる関数"
 
     html_color = '#%02X%02X%02X' % (
         255, int(255*(1 - attn)), int(255*(1 - attn)))
     return '<span style="background-color: {}"> {}</span>'.format(html_color, word)
+
+def load_json():
+    with open("./utils/data/labels.json", "r") as f:
+      labels = json.load(f)
+    return labels
 
 def mk_html(index, sample, preds, attn_weight1, word2index):
   index2word = {}
@@ -12,18 +18,12 @@ def mk_html(index, sample, preds, attn_weight1, word2index):
 
   sentence = sample["input_ids"][index]
   labels = sample["labels"][index]
-  pred = preds[index]
+  pred = preds[index].item()
   # batchを指定して<cls>と各トークンの重みを取り出す
   attns1 = attn_weight1[index, 0, :]
 
-  if labels == 0:
-    label_str = "Negative"
-  else:
-    label_str = "Positive"
-  if pred == 0:
-    pred_str = "Negative"
-  else:
-    pred_str = "Positive"
+  corr = load_json()
+  label_str = corr[str(pred)]
 
   assert sentence.size()[0] == attns1.size()[0]
 
